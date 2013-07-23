b5b07705e196da375cb91fb2581c463cc7573122
pauly <pauly@clarkeology.com>
2013-07-23 21:47:09 +0000

    return retval

--- init.d
+++ init.d
@@ -1,14 +1,34 @@
 #!/bin/sh
 
+#!/usr/bin/env ruby
+#
+# app_name      This is a startup script for use in /etc/init.d 
+# 
+# chkconfig:    2345 80 20 
+# description:  Description of program / service  
+# APP_NAME = 'app_name'  
+# case ARGV.first     
+#   when 'status':     	   
+#     status = 'stopped'         
+#     puts "#{APP_NAME} is #{status}"
+#   when 'start': 		# Do your thang
+#   when 'stop': 		# Do your thang
+#   when 'restart': 		# Do your thang		
+#   else
+#     puts "Usage: #{APP_NAME} {start|stop|restart}"
+#     exit 
+# end
+
 # init.d file
-# so copy this file to /etc/init.d/ and restart your pi
-# requires node
-# see https://github.com/pauly/lightwaverf
+# so copy this file to /etc/init.d/lightwaverf-api and restart your pi
+# requires ruby
+# see https://github.com/pauly/lightwaverf-api
 
 ROOT_DIR="/home/pi/lightwaverf-api"
 SERVER="$ROOT_DIR/api.rb"
 LOG_FILE="$ROOT_DIR/api.log"
 CONFIG_FILE="$ROOT_DIR/config/default.sh"
+LIGHTWAVERF_API_PORT=80
 
 # LOCK_FILE="/var/lock/lightwaverf-api"
 
@@ -20,10 +40,10 @@ do_start() {
     else
       echo "config file $CONFIG_FILE is missing..."
     fi
-    $SERVER -p 80 -e production >> $LOG_FILE &
+    $SERVER -p $LIGHTWAVERF_API_PORT -e production > $LOG_FILE 2>&1 &
     RETVAL=$?
-    echo
-    [ $RETVAL -eq 0 ] && touch $LOCK_FILE
+    # echo
+    # [ $RETVAL -eq 0 ] && touch $LOCK_FILE
   # else
     # echo "$SERVER is locked ($LOCK_FILE)."
     # RETVAL=1
@@ -32,9 +52,9 @@ do_start() {
 
 do_stop() {
   echo -n $"Stopping $SERVER: "
-  pid=`ps -aefw | grep "$DAEMON $SERVER" | grep -v " grep " | awk '{print $2}'`
+  pid=`ps -aefw | grep $SERVER | grep -v " grep " | awk '{print $2}'`
   kill -9 $pid > /dev/null 2>&1
-  # RETVAL=$?
+  RETVAL=$?
   # [ $RETVAL -eq 0 ] && rm -f $LOCK_FILE
 }
 
